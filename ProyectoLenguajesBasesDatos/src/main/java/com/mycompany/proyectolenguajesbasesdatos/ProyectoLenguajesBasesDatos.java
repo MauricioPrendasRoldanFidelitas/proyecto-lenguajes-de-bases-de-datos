6efaf2cf6a669b624f306489363845430bc6df81
MauricioPrendasRoldanFidelitas <mprendas20766@ufide.ac.cr>
2025-07-13 20:58:48 -0700

    Creación del proyecto de Java

---
+++ ProyectoLenguajesBasesDatos/src/main/java/com/mycompany/proyectolenguajesbasesdatos/ProyectoLenguajesBasesDatos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectolenguajesbasesdatos;
+
+/**
+ *
+ * @author fidelitas
+ */
+public class ProyectoLenguajesBasesDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
